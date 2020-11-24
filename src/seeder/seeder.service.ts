f74333946b5bf699cf2b675f3cab9d91d1cc4373
Hannah Vivian Shaw <hvivianshaw@gmail.com>
2020-11-23 20:32:28 -0500

    seed vehicles

--- src/seeder/seeder.service.ts
+++ src/seeder/seeder.service.ts
@@ -1,17 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { Vehicle } from 'src/vehicles/vehicle.model';
 import { VehiclesService } from 'src/vehicles/vehicles.service';
+import { VEHICLES } from './data/vehicles.json';
 
 @Injectable()
 export class SeederService {
   constructor(private readonly vehiclesService: VehiclesService) {}
 
   async seed() {
-    const testV = new Vehicle();
-    testV.name = 'Test V';
-    testV.description = 'Desc';
-    testV.length = 10;
-    testV.vehicle_class = 'Test';
-    return await this.vehiclesService.save(testV);
+    for (const { id, name, description, vehicle_class, length } of VEHICLES) {
+      const seedVehicle = new Vehicle();
+      seedVehicle.id = id;
+      seedVehicle.name = name;
+      seedVehicle.description = description;
+      seedVehicle.vehicle_class = vehicle_class;
+      seedVehicle.length = length;
+      await this.vehiclesService.save(seedVehicle);
+    }
   }
 }
