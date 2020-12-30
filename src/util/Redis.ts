86c7be81bbef3671fb019305865e6dc90d0794fd
Hannah Vivian Shaw <hvivianshaw@gmail.com>
2020-12-30 04:00:52 -0500

    fix Redis url loading

--- src/util/Redis.ts
+++ src/util/Redis.ts
@@ -1,5 +1,9 @@
 import { RedisCache } from 'apollo-server-cache-redis';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+console.log(process.env.REDIS_URL);
 const Redis = new RedisCache(process.env.REDIS_URL);
 
 export default Redis;
